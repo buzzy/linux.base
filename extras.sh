eb62f6e7555bfafb05822cdeed847a1d4c49fcee
buzzy <daniele.testa@gmail.com>
2019-07-02 21:58:24 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -4,6 +4,6 @@ cd /opt
 wget https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2017.3.23.tgz
 tar zxfv ntfs-3g_ntfsprogs-2017.3.23.tgz
 cd ntfs-3g_ntfsprogs-2017.3.23
-./configure --prefix=/opt/sysroot --host=arm-linux-gnueabi --mandir=/dev/null --docdir=/dev/null
+./configure --prefix=/opt/sysroot --host=arm-linux-gnueabi --mandir=/tmp --docdir=/tmp
 make -j$(nproc)
 make install
