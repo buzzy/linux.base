fefdef84c39340cf2536fc7956c6d7bdd9cd0b00
buzzy <daniele.testa@gmail.com>
2019-07-03 19:39:22 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#PREPARE
 cd /opt
-wget https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2017.3.23.tgz
-tar zxfv ntfs-3g_ntfsprogs-2017.3.23.tgz
-cd ntfs-3g_ntfsprogs-2017.3.23
 export CC=arm-linux-gnueabihf-gcc
-./configure --prefix=/opt/sysroot/usr --host=arm-linux-gnueabi --mandir=/tmp --docdir=/tmp --disable-static --with-fuse=internal --disable-ntfsprogs --exec-prefix=/opt/sysroot/usr
-make -j$(nproc)
-make install
+
+#NTFS-3G
+#wget https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2017.3.23.tgz
+#tar zxfv ntfs-3g_ntfsprogs-2017.3.23.tgz
+#cd ntfs-3g_ntfsprogs-2017.3.23
+#./configure --prefix=/opt/sysroot/usr --host=arm-linux-gnueabi --mandir=/tmp --docdir=/tmp --disable-static --with-fuse=internal --disable-ntfsprogs --exec-prefix=/opt/sysroot/usr
+#make -j$(nproc)
+#make install
