7974dd7f794610762ac289667176ca10704d8df8
buzzy <daniele.testa@gmail.com>
2019-07-05 22:51:20 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -10,6 +10,6 @@ apt-get -y install pkg-config libfuse-dev
 wget https://github.com/relan/exfat/releases/download/v1.3.0/fuse-exfat-1.3.0.tar.gz
 tar xfv fuse-exfat-1.3.0.tar.gz
 cd fuse-exfat-1.3.0
-./configure --prefix=/opt/sysroot --target=arm-linux-gnueabihf
+./configure --prefix=/opt/sysroot --host=arm-linux-gnueabihf
 make -j$(nproc)
 make install
