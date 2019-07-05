a021da9ef31cf1cf8edd4a3b3ea2c35651f1bdb1
buzzy <daniele.testa@gmail.com>
2019-07-06 02:06:52 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -15,7 +15,7 @@ cd fuse-2.9.9
   --datarootdir=/tmp \
   --disable-static
   
-make -j$(nproc) CFLAGS="--sysroot=/opt/sysroot"
+make -j$(nproc) CFLAGS="-O2 --sysroot=/opt/sysroot"
 make install
 
 #libfuse 3
@@ -42,6 +42,6 @@ apt-get -y install pkg-config libfuse-dev
 wget https://github.com/relan/exfat/releases/download/v1.3.0/fuse-exfat-1.3.0.tar.gz
 tar xfv fuse-exfat-1.3.0.tar.gz
 cd fuse-exfat-1.3.0
-./configure --prefix=/opt/sysroot --host=arm-linux-gnueabihf --datarootdir=/tmp
+./configure CFLAGS="-O2 --sysroot=/opt/sysroot" --prefix=/opt/sysroot --host=arm-linux-gnueabihf --datarootdir=/tmp
+make -j$(nproc)
 make install
-make -j$(nproc) CFLAGS="--sysroot=/opt/sysroot"
