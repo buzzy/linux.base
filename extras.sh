2e6639a55bc49699065482815a4358320c1f824f
buzzy <daniele.testa@gmail.com>
2019-07-06 21:24:02 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -9,7 +9,7 @@ tar xfv fuse-2.9.9.tar.gz
 cd fuse-2.9.9
 
 ./configure \
-  CFLAGS="-O2 --sysroot=/opt/sysroot" \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
   --prefix=/opt/sysroot/usr \
   --host=arm-linux-gnueabihf \
   --datarootdir=/tmp \
@@ -43,7 +43,7 @@ wget https://github.com/relan/exfat/releases/download/v1.3.0/fuse-exfat-1.3.0.ta
 tar xfv fuse-exfat-1.3.0.tar.gz
 cd fuse-exfat-1.3.0
 ./configure \
-  CFLAGS="-O2 --sysroot=/opt/sysroot" \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
   --prefix=/opt/sysroot \
   --host=arm-linux-gnueabihf \
   --datarootdir=/tmp
@@ -65,7 +65,7 @@ wget https://github.com/thom311/libnl/releases/download/libnl3_4_0/libnl-3.4.0.t
 tar xfv libnl-3.4.0.tar.gz
 cd libnl-3.4.0
 ./configure \
-  CFLAGS="-O2 --sysroot=/opt/sysroot" \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
   --host=arm-linux-gnueabihf \
   --prefix=/opt/sysroot/usr \
   --sysconfdir=/opt/sysroot/etc \
@@ -80,4 +80,4 @@ make install
 #wget https://www.kernel.org/pub/software/network/iw/iw-5.0.1.tar.xz
 #tar xfv iw-5.0.1.tar.xz
 #cd iw-5.0.1
-#make CC=arm-linux-gnueabihf-gcc -j$(nproc)
+#make CFLAGS="-O2 -s" CC=arm-linux-gnueabihf-gcc -j$(nproc)
