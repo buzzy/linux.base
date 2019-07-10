dd5d207c9a824da556bbd39c12cc0b86091719af
buzzy <daniele.testa@gmail.com>
2019-07-10 23:31:43 +0800

    Update wayland.sh

--- wayland.sh
+++ wayland.sh
@@ -1,6 +1,71 @@
 #!/bin/sh
+set -e
+set -x
 
+#libffi
+cd /opt
+wget https://sourceware.org/ftp/libffi/libffi-3.2.1.tar.gz
+tar xfv libffi-3.2.1.tar.gz
+cd libffi-3.2.1
+sed -e '/^includesdir/ s/$(libdir).*$/$(includedir)/' \
+    -i include/Makefile.in
+sed -e '/^includedir/ s/=.*$/=@includedir@/' \
+    -e 's/^Cflags: -I${includedir}/Cflags:/' \
+    -i libffi.pc.in
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/usr \
+  --datarootdir=/tmp \
+  --enable-static=no
+make -j$(nproc)
+make DESTDIR=/opt/sysroot install
+rm -rf /opt/sysroot/tmp/*
+
+#expat
+cd /opt
+wget https://github.com/libexpat/libexpat/releases/download/R_2_2_7/expat-2.2.7.tar.xz
+tar xfv expat-2.2.7.tar.xz
+cd expat-2.2.7
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/usr \
+  --datarootdir=/tmp \
+  --without-examples \
+  --without-tests \
+  --enable-static=no
+make -j$(nproc)
+make DESTDIR=/opt/sysroot install
+rm -rf /opt/sysroot/tmp/*
+
+#libxml
+cd /opt
+wget http://xmlsoft.org/sources/libxml2-2.9.9.tar.gz
+tar xfv libxml2-2.9.9.tar.gz
+cd libxml2-2.9.9
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/usr \
+  --enable-static=no
+make -j$(nproc)
+make DESTDIR=/tmp/libxml install
+rm -rf /tmp/libxml/usr/share/{doc,gtk-doc,man}
+cp -rv /tmp/libxml/* /opt/sysroot
+rm -rf /tmp/libxml
+
+#wayland
 cd /opt
 wget https://wayland.freedesktop.org/releases/wayland-1.17.0.tar.xz
 tar xfv wayland-1.17.0.tar.xz
 cd wayland-1.17.0
+./configure \
+  CFLAGS="-O2 -s --sysroot=/opt/sysroot" \
+  PKG_CONFIG_DIR="" \
+  PKG_CONFIG_LIBDIR="/opt/sysroot/usr/lib/pkgconfig" \
+  PKG_CONFIG_SYSROOT_DIR="/opt/sysroot" \
+  --host=arm-linux-gnueabihf \
+  --prefix=/usr \
+  --disable-documentation
+make CFLAGS="--sysroot=-/opt/sysroot -I/opt/sysroot/usr/include -L/opt/sysroot/usr/lib" -j$(nproc)
